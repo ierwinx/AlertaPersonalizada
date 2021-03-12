fa637d1bfb100123cc771f8df15209c055ea1ca8
Erwin <ierwinx@gmail.com>
2021-03-12 10:09:45 -0600

    se agrega sombra

--- AlertaPersonalizada/AlertaViewController.swift
+++ AlertaPersonalizada/AlertaViewController.swift
@@ -1,9 +1,16 @@
 import UIKit
 
 class AlertaViewController: UIViewController {
-
+    
+    @IBOutlet weak var vista: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        vista.layer.shadowRadius = 5
+        vista.layer.shadowOpacity = 1
+        vista.layer.shadowColor = UIColor.black.cgColor
+        
     }
     
     @IBAction func cerrar(_ sender: Any) {
